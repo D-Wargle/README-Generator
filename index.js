6dcbeed15944941fcb1bd41431538805fec95312
Daniel T <Danieltwarg@gmail.com>
2024-10-07 12:04:54 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,70 @@
+// TODO: Include packages needed for this application
+import inquirer from 'inquirer';
+import fs from 'fs';
+import generateMarkdown from './develop/utils/generateMarkdown.js';
+// TODO: Create an array of questions for user input
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please provide a description of the project',
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Please provide installation instructions',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Please provide usage information',
+    },
+    {
+        type: 'input',
+        name: 'contribution',
+        message: 'Please provide contribution guidelines',
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: 'Please provide test instructions',
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'Please provide your GitHub username',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Please provide your email address',
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Please select a license for your project',
+        choices: ['MIT', 'GNU GPLv3', 'Apache 2.0', 'ISC'],
+    },
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) =>
+        err ? console.error(err) : console.log('Success!')
+    );
+}
+
+// TODO: Create a function to initialize app
+function init() { 
+    inquirer.prompt(questions)
+    .then((data) => {
+        writeToFile('README.md', generateMarkdown(data));
+    })
+}
+// Function call to initialize app
+init();
